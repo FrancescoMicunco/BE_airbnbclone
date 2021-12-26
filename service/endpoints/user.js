ebb234a67842cb5f1aa24928db5aa3c1c4dfd263
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-26 08:36:33 +0100

    put, delete, getid

--- service/endpoints/user.js
+++ service/endpoints/user.js
@@ -25,6 +25,56 @@ router
         }
     })
 
+router
+    .route('/:id')
+    .get(async(req, res, next) => {
+        try {
+            const user = await User.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            if (user === null) { "this user doesn't exist" } else {
+                res.send(user)
+            }
+
+        } catch (error) {
+            next(error)
+        }
+    })
+
+.put(async(req, res, next) => {
+    try {
+        const user = await User.update(req.body, {
+            where: {
+                id: req.params.id
+            },
+            returning: true
+        })
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+
+.delete(async(req, res, next) => {
+    try {
+        const user = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        if (user > 0) {
+            res.send("201 - user deleted!")
+        } else { res.send("User not found!") }
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+
 
 
 export default router
