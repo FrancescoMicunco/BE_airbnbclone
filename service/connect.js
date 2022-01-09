9333bb5d3175b5411bb0b01baa8159b12b0e339d
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-09 09:35:03 +0100

    create new connection ready for deploy

--- service/connect.js
+++ service/connect.js
@@ -1,6 +1,24 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.DB_URL)
+const sequelize = new Sequelize(
+    // process.env.DB_URL 
+    process.env.DATABASE,
+    process.env.USER,
+    process.env.PASSWORD, {
+        port: process.env.DB_PORT,
+        host: process.env.HOST,
+        dialect: "postgres",
+        //or the longer string
+        // dialectOptions: { // IMPORTANT
+        //     ssl: {
+        //         require: false,
+        //         rejectUnauthorized: false,
+        //     },
+        // },
+    }
+
+
+)
 
 export const testDB = async() => {
     try {
