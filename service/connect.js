6137a169d01c9f5e391167b26d52b8a260e8ef74
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-09 12:17:58 +0100

    connection issues fixed

--- service/connect.js
+++ service/connect.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(
-    process.env.DB_URL, {
+    process.env.DATABASE_URL, {
         dialectOptions: {
             ssl: {
                 require: true,
